0c47cbffddb875794c892cce458fe1b392bcbad0
Sushruth Chandrasekar <sushruth31@gmail.com>
2022-02-14 20:01:27 -0600

    animations

--- frontend/src/buttons.js
+++ frontend/src/buttons.js
@@ -1,21 +1,27 @@
 import { Button } from "@mui/material";
-import { useEffect } from "react";
 import { useRecoilValue } from "recoil";
-import { topCardRefTodosAtom, topCardRefDoneAtom } from "./atoms/cardrefatom";
+import { doneAtom, todosAtom } from "./atoms/dataatom";
+import useSwipes from "./useSwipes";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 export default function () {
-  const topcardreftodo = useRecoilValue(topCardRefTodosAtom);
-  const topcardrefdone = useRecoilValue(topCardRefDoneAtom);
-
-  useEffect(() => console.log(topcardreftodo), [topcardreftodo]);
+  const todos = useRecoilValue(todosAtom);
+  const done = useRecoilValue(doneAtom);
+  const todosCopy = [...todos];
+  const doneCopy = [...done];
+  const [_, onSwipeRight] = useSwipes(todosCopy.pop());
+  const [onSwipeLeft, __] = useSwipes(doneCopy.pop());
 
   return (
     <div style={{ flex: "0.1" }} className="flex h-screen flex-col items-center justify-center">
       <div className="flex h-[20%] w-[100%] flex-col items-center justify-around">
-        <Button onClick={() => topcardreftodo.swipe("right")} variant="contained">
+        <Button onClick={onSwipeRight} variant="contained">
           Add
+          {<KeyboardArrowRightIcon />}
         </Button>
-        <Button onClick={() => topcardrefdone.swipe("left")} variant="outlined">
+        <Button onClick={onSwipeLeft} variant="outlined">
+          {<KeyboardArrowLeftIcon />}
           Remove
         </Button>
       </div>
--- frontend/src/todo.js
+++ frontend/src/todo.js
@@ -1,16 +1,17 @@
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import Card from "./card";
 import { asset, todosAtom, doneAtom } from "./atoms/dataatom";
+import { searchAtom } from "./atoms/searchatom";
 import { useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function ({ todoOrDone }) {
   const fetchedData = asset.read("http://localhost:8080/cards");
   const [todos, setTodosData] = useRecoilState(todosAtom);
   const [done, setDoneData] = useRecoilState(doneAtom);
   const dataToMap = todoOrDone === "todo" ? todos : done;
-
+  const searchText = useRecoilValue(searchAtom);
   const className1 = `flex  h-screen flex-col items-center justify-center border-r border-gray-400`;
-
   const className2 = `flex  h-screen flex-col items-center justify-center border-l border-gray-400`;
 
   useEffect(() => {
@@ -18,17 +19,39 @@ export default function ({ todoOrDone }) {
     setDoneData(fetchedData.filter(({ status }) => status === "DONE"));
   }, []);
 
+  //search function
+
+  useEffect(() => {
+    //filter out the ones in the done list also
+
+    setTodosData(
+      fetchedData.filter(
+        ({ patient_name }) =>
+          patient_name.toLowerCase().includes(searchText.toLowerCase()) &&
+          !done.map(el => el.patient_name).includes(patient_name)
+      )
+    );
+  }, [searchText]);
+
   return (
     <div style={{ flex: "0.45" }} className={todoOrDone === "todo" ? className1 : className2}>
       <div className="-mt-[600px] text-xl font-bold text-black">{todoOrDone === "todo" ? "To Do" : "Done"}</div>
-      {dataToMap.map((obj, i) => (
-        <Card
-          isTopCard={todoOrDone === "todo" ? i === todos.length - 1 : i === done.length - 1}
-          todoOrDone={todoOrDone}
-          key={obj.id}
-          {...obj}
-        />
-      ))}
+      {dataToMap.length > 0 ? (
+        dataToMap.map((obj, i) => {
+          return <Card topCard todoOrDone={todoOrDone} key={obj.id} {...obj} />;
+        })
+      ) : (
+        <AnimatePresence>
+          <motion.div
+            key={"nomorecards"}
+            initial={{ width: 0 }}
+            animate={{ width: 300 }}
+            exit={{ width: 0 }}
+            className="absolute mb-[50px] flex h-52 items-center justify-center rounded-xl bg-blue-300">
+            No More Cards
+          </motion.div>
+        </AnimatePresence>
+      )}
     </div>
   );
 }
