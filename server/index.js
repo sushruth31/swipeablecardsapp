67c81e07ad0099447b5c573bab921406acf530d9
Sushruth Chandrasekar <sushruth31@gmail.com>
2022-02-18 20:41:12 -0600

    feat: serve generic card fixtures from a configurable mock api
    Drop the hand-rolled /cards handler in favour of json-server's own router, and
    exit with a named error when API_PORT is absent instead of hardcoding the port.

--- server/index.js
+++ server/index.js
@@ -1,23 +1,19 @@
+require("dotenv").config();
+
 const jsonServer = require("json-server");
+const cards = require("./cards.json");
+
+const port = Number(process.env.API_PORT);
+
+if (!Number.isInteger(port) || port <= 0) {
+  process.stderr.write("API_PORT is missing or not a port number. Copy .env.example to .env and set it.\n");
+  process.exit(1);
+}
+
+// json-server builds the REST routes (and permissive CORS) from this shape,
+// so GET /cards serves the fixture array with no hand-written handler.
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const fs = require("fs");
-const path = require("path");
+server.use(jsonServer.defaults());
+server.use(jsonServer.router({ cards }));
 
-// Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares);
-
-// Add custom routes before JSON Server router
-server.get("/cards", (req, res) => {
-  const file = fs.readFileSync(path.join(__dirname, "cards.json"));
-  res.jsonp(JSON.parse(file));
-});
-
-server.use(jsonServer.bodyParser);
-
-// Use default router
-server.use(router);
-server.listen(8080, () => {
-  console.log("JSON Server is running port 8080");
-});
+server.listen(port, () => process.stdout.write(`card api listening on http://localhost:${port}/cards\n`));
