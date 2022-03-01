bd30a2af73a967a9f491b2c9fbe41f7606596b74
Sushruth Chandrasekar <sushruth31@gmail.com>
2022-02-28 19:08:41 -0600

    fix: hoist AnimatePresence out of the empty-column placeholder
    The placeholder declared its own AnimatePresence, so the wrapper unmounted
    together with its only child and the exit variant never had a chance to run.
    Only initial and animate were ever live. Moving it up to wrap the whole
    placeholder/cards branch makes the collapse animate on both edges.

--- frontend/src/components/CardColumn.js
+++ frontend/src/components/CardColumn.js
@@ -6,19 +6,9 @@ import { Card } from "./Card";
 const HEADINGS = { pending: "To do", done: "Done" };
 const EDGES = { pending: "border-r", done: "border-l" };
 
-function EmptyColumn() {
-  return (
-    <AnimatePresence>
-      <motion.div
-        initial={{ width: 0 }}
-        animate={{ width: 300 }}
-        exit={{ width: 0 }}
-        className="absolute mb-[50px] flex h-52 items-center justify-center rounded-xl bg-blue-300">
-        No more cards
-      </motion.div>
-    </AnimatePresence>
-  );
-}
+const SECTION_STYLE = "flex h-screen flex-col items-center justify-center border-gray-400";
+const PLACEHOLDER_STYLE = "absolute mb-[50px] flex h-52 items-center justify-center rounded-xl bg-blue-300";
+const PLACEHOLDER = { initial: { width: 0 }, animate: { width: 300 }, exit: { width: 0 } };
 
 export function CardColumn({ column }) {
   const { done } = useRecoilValue(boardAtom);
@@ -26,15 +16,19 @@ export function CardColumn({ column }) {
   const cards = column === "pending" ? pending : done;
 
   return (
-    <section
-      style={{ flex: "0.45" }}
-      className={`flex h-screen flex-col items-center justify-center border-gray-400 ${EDGES[column]}`}>
+    <section style={{ flex: "0.45" }} className={`${SECTION_STYLE} ${EDGES[column]}`}>
       <h2 className="-mt-[600px] text-xl font-bold text-black">{HEADINGS[column]}</h2>
-      {cards.length === 0 ? (
-        <EmptyColumn />
-      ) : (
-        cards.map(card => <Card key={card.id} column={column} {...card} />)
-      )}
+      {/* AnimatePresence has to sit outside the ternary: wrapped around the
+          placeholder alone it would unmount with it and never run the exit. */}
+      <AnimatePresence initial={false}>
+        {cards.length === 0 ? (
+          <motion.div key="empty" {...PLACEHOLDER} className={PLACEHOLDER_STYLE}>
+            No more cards
+          </motion.div>
+        ) : (
+          cards.map(card => <Card key={card.id} column={column} {...card} />)
+        )}
+      </AnimatePresence>
     </section>
   );
 }
